7689ddee9bed7a7de03345593aca11eab58d167d
Justynn Hammond <59801521+jhamm2315@users.noreply.github.com>
2021-02-27 10:57:02 -0700

    Update app.py

--- SQLAlchemy/app.py
+++ SQLAlchemy/app.py
@@ -3,7 +3,7 @@ import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, fun
 
 from flask import Flask, jsonify
 
